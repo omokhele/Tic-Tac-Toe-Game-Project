048f7a68bb0f0378439445a30248a8078302fd30
omokhele <anne.imomoh@gmail.com>
2022-02-15 12:17:48 -0800

    effected changes in css
    skeletal work in js file

--- script.js
+++ script.js
@@ -8,30 +8,34 @@ const reset = document.querySelector(".restart")
 const nameField = document.querySelector("#nameField");
 const play = document.querySelector(".play");
 
-// // let userName = window.prompt("What's your name?");
+// let userName = window.prompt("What's your name?");
 
-// document.getElementById("enter1").onclick = function(){
-//     // let userName = nameField.value
-//     let userName1 = document.getElementById("nameField1").value;
+document.getElementById("enter1").onclick = function(){
+    // let userName = nameField.value
+    let userName1 = document.getElementById("nameField1").value;
 
-//     console.log("Hello " + userName1);
-// }
-// document.getElementById("enter2").onclick = function(){
-// let userName2 = document.getElementById("nameField2").value;
-//     console.log("Hello " + userName2);
-// }
+    console.log("Hello " + userName1);
+}
+document.getElementById("enter2").onclick = function(){
+let userName2 = document.getElementById("nameField2").value;
+    console.log("Hello " + userName2);
+}
 
 // const board = Array(gridItem.length);
 // console.log(board);
 
-gridItems.forEach((gridItem) => gridItem.addEventListener(click, () => {
-    let add = playerX + add;
+// gridItems.forEach((gridItem) => gridItem.addEventListener(click, () => {
+//     let add = playerX + add;
 
+for (let i = 0; i < 9; i++) {
+    
     if(playerTurn === playerX){
     gridItem.innerText = playerX;
-
-    }else (playerTurn === playerO);
-
-    if gr
-
-})
+    }
+    else if (playerTurn === playerO) {
+        gridItem.innerText = playerO;
+    
+    }
+    
+}
+        
