a443865dac6b737c70dbf8d1f00cfe090e697cec
omokhele <anne.imomoh@gmail.com>
2022-02-15 11:01:37 -0800

    New Changes

--- script.js
+++ script.js
@@ -1,1 +1,37 @@
-const button = button.addEventListener
+let playerX = "X";
+let playerO = "O";
+
+let playerTurn;
+
+const gridItem = document.querySelectorAll(".gridItem");
+const reset = document.querySelector(".restart")
+const nameField = document.querySelector("#nameField");
+const play = document.querySelector(".play");
+
+// // let userName = window.prompt("What's your name?");
+
+// document.getElementById("enter1").onclick = function(){
+//     // let userName = nameField.value
+//     let userName1 = document.getElementById("nameField1").value;
+
+//     console.log("Hello " + userName1);
+// }
+// document.getElementById("enter2").onclick = function(){
+// let userName2 = document.getElementById("nameField2").value;
+//     console.log("Hello " + userName2);
+// }
+
+// const board = Array(gridItem.length);
+// console.log(board);
+
+gridItems.forEach((gridItem) => gridItem.addEventListener(click, () => {
+    let add = playerX + add;
+
+    if(playerTurn === playerX){
+    gridItem.innerText = playerX;
+
+    }else (playerTurn === playerO);
+
+    if gr
+
+})
