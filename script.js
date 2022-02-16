853770889c54f514643f050bbd298a87bd163569
omokhele <anne.imomoh@gmail.com>
2022-02-15 19:09:35 -0800

    Saving New Changes

--- script.js
+++ script.js
@@ -18,25 +18,25 @@ let gameLost = document.querySelector("#gameLost");
 
 // Getting Name Input
 
-document.getElementById("enter1").onclick = function(){
-    // let userName = nameField.value
-    let userName1 = document.getElementById("nameField1").value;
-    // console.log("Hello " + userName1);
-}
+// document.getElementById("enter1").onclick = function(){
+//     // let userName = nameField.value
+//     let userName1 = document.getElementById("nameField1").value;
+//     // console.log("Hello " + userName1);
+// }
 
-document.getElementById("enter2").onclick = function(){
-let userName2 = document.getElementById("nameField2").value;
-    // console.log("Hello " + userName2);
-}
+// document.getElementById("enter2").onclick = function(){
+// let userName2 = document.getElementById("nameField2").value;
+//     // console.log("Hello " + userName2);
+// }
 
-// const board = Array(gridItem.length);
-// console.log(board);
+const board = Array(gridItem.length);
+console.log(board);
 
 // gridItems.forEach((gridItem) => gridItem.addEventListener(click, () => {
 //     let add = playerX + add;
 
 // Input Xs and Os
-for (let i = 0; i < 9; i++) {
+for (let i = 0; i <gridItem.length; i++) {
     
     if(playerTurn === playerX){
     gridItem.innerText = playerX;
